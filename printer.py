cff7b278de70c8d6a366b3876b6cb05b3488cef7
Lukyky414 <luucka.e@gmail.com>
2020-11-12 22:41:41 +0100

    Error correction

--- printer.py
+++ printer.py
@@ -2,7 +2,7 @@ from math import sqrt, pi
 from sys import stderr
 
 class Printer:
-    def __init__():
+    def __init__(self):
         #Parameters of the printer
         self.TEMP_HOT_END = 200
         self.TEMP_BED = 70
@@ -37,8 +37,7 @@ class Printer:
     
     #Move the hot end to position.
     # x, y, z -> the position. None : no change in axes
-    
-    def go_to(x=None, y=None, z=None):
+    def go_to(self, x=None, y=None, z=None):
         if self._file is None:
             print("Please create a file with new_file before anything else", file=stderr)
             exit(1)
@@ -80,7 +79,7 @@ class Printer:
 
 
     #Move the hot end to position and print plastic
-    def print_to(x=None, y=None, z=None):
+    def print_to(self, x=None, y=None, z=None):
         if self._file is None:
             print("Please create a file with new_file before anything else", file=stderr)
             exit(1)
@@ -92,13 +91,13 @@ class Printer:
             z = self._z
         
         d = self._distance(x, y, z)
-        e = self._e + self._extruder_position(d)
+        e = self._extruder_position(d)
 
 
         self._go_to(x, y, z, e, self.PRINT_SPEED)
     
     #Move up to a new layer
-    def new_layer():
+    def new_layer(self):
         if self._first_layer:
             self._first_layer = False
             if self.ENABLE_FAN:
@@ -106,8 +105,8 @@ class Printer:
         
         self._go_to(self._x, self._y, self._z+self.LAYER_HEIGHT, self._e, self.MOVEMENT_SPEED)
     
-    def _go_to(x, y, z, e, speed):
-        print("G1 X"+x+" Y"+y+" Z"+z+" E"+e+" F"+speed, file=self._file)
+    def _go_to(self, x, y, z, e, speed):
+        print("G1 X{:.9f} Y{:.9f} Z{:.9f} E{:.9f} F{}".format(x, y, z, e, speed), file=self._file)
 
         self._x = x
         self._y = y
@@ -115,13 +114,13 @@ class Printer:
         self._e = e
 
     
-    def _distance(x, y, z):
+    def _distance(self, x, y, z):
         tot = (self._x - x)**2 + (self._y - y)**2 + (self._z - z)**2
         return sqrt(tot)
 
 
     #Get the position needed for the hot end to print
-    def _extruder_position(distance):
+    def _extruder_position(self, distance):
         if self._filament_surface is None:
             self._filament_surface = pi * self.FILAMENT_DIAMETER**2
         
@@ -132,12 +131,12 @@ class Printer:
         return self._e + size_to_print
 
     #Create a new file, and automatically add the header
-    def new_file(name):
+    def new_file(self, name):
         self._file = open(name, "w")
         self._file_header()
 
     #The header of the file
-    def _file_header():
+    def _file_header(self):
         #Absolute positionning
         print("G90", file=self._file)
 
@@ -147,11 +146,11 @@ class Printer:
         self._y = 0
 
         #Select the temperature needed
-        print("M104 S"+self.TEMP_HOT_END, file=self._file)
-        print("M140 S"+self.TEMP_BED, file=self.file)
+        print("M104 S"+str(self.TEMP_HOT_END), file=self._file)
+        print("M140 S"+str(self.TEMP_BED), file=self._file)
         #and wait to heat up
-        print("M109 S"+self.TEMP_HOT_END, file=self._file)
-        print("M190 S"+self.TEMP_BED, file=self.file)
+        print("M109 S"+str(self.TEMP_HOT_END), file=self._file)
+        print("M190 S"+str(self.TEMP_BED), file=self._file)
 
         #Home all axes (before the print, in case of heat distortion)
         print("G28 X0 Y0 Z0", file=self._file)
@@ -166,9 +165,9 @@ class Printer:
         #Print a fat straight line to purge the Hot end
         self._go_to(0, self.FAT_LINE_POSITION_Y, self.LAYER_HEIGHT, 0, 1500)
 
-        d = self._distance(self.BED_SIZE_X, self.FAT_LINE_POSITION_Y, self.LAYER_HEIGHT)
+        d = self._distance(self.BED_SIZE_X/2, self.FAT_LINE_POSITION_Y, self.LAYER_HEIGHT)
         e = self._extruder_position(d)*2
-        self._go_to(self.BED_SIZE_X, self.FAT_LINE_POSITION_Y, self.LAYER_HEIGHT, e, 900)
+        self._go_to(self.BED_SIZE_X/2, self.FAT_LINE_POSITION_Y, self.LAYER_HEIGHT, e, 900)
 
         #Reset the position of the extruder (begin file with e = 0)
         print("G92 E0", file=self._file)
@@ -178,7 +177,7 @@ class Printer:
         self._filament_surface = None
 
     #End and close the file
-    def end_file():
+    def end_file(self):
         if self._file is None:
             print("Please create a file with new_file before anything else", file=stderr)
             exit(1)
@@ -187,7 +186,7 @@ class Printer:
         self._file = None
 
     #The footer of the file
-    def _file_footer():
+    def _file_footer(self):
         #disable the fan
         print("M107", file=self._file)
 
@@ -195,7 +194,7 @@ class Printer:
         self._go_to(self._x, self._y, self._z, self._e-self.RETRACT_DISTANCE, self.RETRACT_SPEED)
 
         #Go up a bit to avoid collision with the print
-        self._go_to(self._x, self._y, self._z+self.Z_LIFTING, self._e, 1500)
+        self._go_to(self._x, self._y, self._z+1, self._e, 1500)
 
         #Home x and y axis to reset position
         print("G28 X0 Y0", file=self._file)
